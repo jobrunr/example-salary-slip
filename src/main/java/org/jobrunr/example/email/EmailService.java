7bf5f202fda00dcb645d3b1fc4c8c7bfe7072857
Ronald Dehuysser <ronald.dehuysser@gmail.com>
2023-08-04 17:32:20 +0200

    Update to latest versions.

--- src/main/java/org/jobrunr/example/email/EmailService.java
+++ src/main/java/org/jobrunr/example/email/EmailService.java
@@ -6,8 +6,8 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 
-import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import java.nio.file.Path;
 
 @Component
--- src/main/java/org/jobrunr/example/paycheck/DocumentGenerationService.java
+++ src/main/java/org/jobrunr/example/paycheck/DocumentGenerationService.java
@@ -1,35 +1,90 @@
 package org.jobrunr.example.paycheck;
 
+import org.jobrunr.example.utils.Docx4JStamper;
 import org.docx4j.Docx4J;
-import org.docx4j.openpackaging.exceptions.Docx4JException;
+import org.docx4j.fonts.BestMatchingMapper;
+import org.docx4j.fonts.PhysicalFonts;
 import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
+import org.jobrunr.jobs.context.JobRunrDashboardLogger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-import org.wickedsource.docxstamper.DocxStamper;
-import org.wickedsource.docxstamper.DocxStamperConfiguration;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class DocumentGenerationService {
 
-    public void generateDocument(Path wordTemplatePath, Path pdfOutputPath, Object context) throws IOException, Docx4JException {
-        Files.createDirectories(pdfOutputPath.getParent().toAbsolutePath());
+    private static final Logger LOGGER = new JobRunrDashboardLogger(LoggerFactory.getLogger(DocumentGenerationService.class));
 
-        try(InputStream template = Files.newInputStream(wordTemplatePath); OutputStream out = Files.newOutputStream(pdfOutputPath)) {
-            final ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-            final DocxStamper stamper = new DocxStamperConfiguration().setFailOnUnresolvedExpression(true).build();
-            stamper.stamp(template, context, byteArrayOutputStream);
+    private final String bestMatchingFonts;
 
-            Docx4J.toPDF(WordprocessingMLPackage.load(new ByteArrayInputStream(byteArrayOutputStream.toByteArray())), out);
-            System.out.println(String.format("Generated salary slip %s", pdfOutputPath)); // for demo purposes only
+    public DocumentGenerationService() {
+        bestMatchingFonts = getBestMatchingFonts();
+        PhysicalFonts.setRegex(bestMatchingFonts);
+    }
+
+
+    public void generateDocument(Path wordTemplatePath, Path pdfOutputPath, SalarySlip salarySlip) throws Exception {
+        if(Files.notExists(pdfOutputPath.getParent().toAbsolutePath())) {
+            Files.createDirectories(pdfOutputPath.getParent().toAbsolutePath());
         }
 
+        try(InputStream templateAsInputStream = Files.newInputStream(wordTemplatePath); OutputStream out = Files.newOutputStream(pdfOutputPath)) {
+            WordprocessingMLPackage template = WordprocessingMLPackage.load(templateAsInputStream);;
+            template.setFontMapper(new BestMatchingMapper());
+
+            Docx4JStamper.searchAndReplace(template, getContext(salarySlip));
+
+            Docx4J.toPDF(template, out);
+            LOGGER.info(String.format("Generated salary slip %s", pdfOutputPath)); // for demo purposes only
+        }
+    }
+
+    private static String getBestMatchingFonts() {
+        String fontRegex = ".*(Courier New|Arial|Times New Roman|Comic Sans|Georgia|Impact|Lucida Console|Lucida Sans Unicode|Palatino Linotype|Tahoma|Trebuchet|Verdana|Symbol|Webdings|Wingdings|MS Sans Serif|MS Serif).*";
+        if(System.getProperty("os.name").startsWith("Windows")) {
+            fontRegex = ".*(calibri|cour|arial|times|comic|georgia|impact|LSANS|pala|tahoma|trebuc|verdana|symbol|webdings|wingding).*";
+        }
+        return fontRegex;
+    }
+
+    private Map<String, String> getContext(SalarySlip salarySlip) {
+        Map<String, String> context = new HashMap<>();
+        context.putAll(getEmployeeDetails(salarySlip));
+        context.putAll(getWorkWeekDetails(salarySlip));
+        context.putAll(getSalaryTotalDetails(salarySlip));
+        return context;
+    }
+
+    private Map<String, String> getEmployeeDetails(SalarySlip salarySlip) {
+        return Map.of(
+                "${employee.id}", salarySlip.getEmployee().getId().toString(),
+                "${employee.firstName}", salarySlip.getEmployee().getFirstName(),
+                "${employee.lastName}", salarySlip.getEmployee().getLastName());
+    }
+
+    private Map<String, String> getWorkWeekDetails(SalarySlip salarySlip) {
+        return Map.of(
+                "${workWeek.weekNbr}", String.valueOf(salarySlip.getWorkWeek().getWeekNbr()),
+                "${workWeek.from}", String.valueOf(salarySlip.getWorkWeek().getFrom()),
+                "${workWeek.to}", String.valueOf(salarySlip.getWorkWeek().getTo()),
+                "${workWeek.workHoursMonday}", String.valueOf(salarySlip.getWorkWeek().getWorkHoursMonday().setScale(2)),
+                "${workWeek.workHoursTuesday}", String.valueOf(salarySlip.getWorkWeek().getWorkHoursTuesday().setScale(2)),
+                "${workWeek.workHoursWednesday}", String.valueOf(salarySlip.getWorkWeek().getWorkHoursWednesday().setScale(2)),
+                "${workWeek.workHoursThursday}", String.valueOf(salarySlip.getWorkWeek().getWorkHoursThursday().setScale(2)),
+                "${workWeek.workHoursFriday}", String.valueOf(salarySlip.getWorkWeek().getWorkHoursFriday().setScale(2)),
+                "${workWeek.total}", String.valueOf(salarySlip.getWorkWeek().getTotal().setScale(2)));
+    }
+
+    private Map<String, String> getSalaryTotalDetails(SalarySlip salarySlip) {
+        return Map.of(
+                "${totalPerHour}", String.valueOf(salarySlip.getTotalPerHour().setScale(2)),
+                "${total}", String.valueOf(salarySlip.getTotal().setScale(2)));
     }
 
 }
